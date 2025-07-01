c57e1a6101effe48d4308d154843ead1763ee5da
c0a2417692 <c0a2417692@edu.teu.ac.jp>
2025-07-01 14:13:24 +0900

    追加機能1実装完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -72,6 +72,7 @@ class Bird(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = xy
         self.speed = 10
+        
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -88,6 +89,12 @@ class Bird(pg.sprite.Sprite):
         引数1 key_lst：押下キーの真理値リスト
         引数2 screen：画面Surface
         """
+
+        if key_lst[pg.K_LSHIFT]:
+            self.speed = 20
+        else:
+            self.speed = 10
+
         sum_mv = [0, 0]
         for k, mv in __class__.delta.items():
             if key_lst[k]:
